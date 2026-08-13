e39d4686bf78d98fe9976f42db28ba2a3c810a71
Arish limbu <limbuarish0@gmail.com>
2026-08-13 21:34:56 +0545

    new

--- EAMS.cpp
+++ EAMS.cpp
@@ -1,2 +1,37 @@
 #include <iostream>
 #include <string.h>
+
+using namespace std;
+
+bool login()
+{
+    string username, password;
+
+    for (int attempt = 1; attempt <= 3; attempt++)
+    {
+        cout << "\n===== LOGIN =====\n";
+
+        cout << "Username: ";
+        cin >> username;
+
+        cout << "Password: ";
+        cin >> password;
+
+        if (username == "admin" && password == "1234")
+        {
+            cout << "\nLogin successful!\n";
+            return true;
+        }
+
+        cout << "\nWrong username or password!\n";
+        cout << "Attempts left: " << 3 - attempt << endl;
+    }
+
+    cout << "\nToo many failed attempts. Access denied!\n";
+    return false;
+}
+int main()
+{
+
+    return 0;
+}
